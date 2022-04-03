89eeaad6d9be9b854c965ab28c114ea24d40a022
xXnielsXx <nielsmichael@comcast.net>
2022-04-03 13:47:10 +0900

    Update routing, add findOne

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//test
-
 //mongoose connection
 mongoose.connect("mongodb://localhost:27017/wikiDB");
 
@@ -24,32 +22,63 @@ const articleSchema = {
 
 const Article = mongoose.model("ArticleSchema", articleSchema);
 
-app.get("/articles", function (req, res) {
-  Article.find(function (err, foundArticles) {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    let x = req.body;
+    console.log(x.title);
+    console.log(x.content);
 
-app.post("/articles", function (req, res) {
-  let x = req.body;
-
-  const jack = new Article({
-    title: x.title,
-    content: x.content,
+    const jack = new Article({
+      title: x.title,
+      content: x.content,
+    });
+    jack.save(function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(
+      //omit conditions to delete all
+      function (err) {
+        if (!err) {
+          res.send("Deleted all articles");
+        } else {
+          console.log(err);
+        }
+      }
+    );
   });
-  jack.save(function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
-});
 
-//establish PORT
-const PORT = process.env.PORT || 3000;
+app
+  .route("/articles/:articleTitle")
+
+  .get(function (req, res) {
+    Article.findOne(
+      { title: req.params.articleTitle },
+      function (err, foundArticle) {
+        if (foundArticle) {
+          res.send(foundArticle);
+        } else {
+          res.send("no articles matching title");
+        }
+      }
+    );
+  });
+
+//establish PORT process.env.PORT ||
+const PORT = 8000;
 
 app.listen(PORT, function () {
   console.log("Server started on port " + PORT);
